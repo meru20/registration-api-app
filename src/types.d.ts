7aaf01a2eeb406db6940e0baed6554b99865b067
meru20 <merhawitgubsa@gmail.com>
2021-05-20 22:03:05 -0700

    feat(GlobalContext):agrabbed state of the app from api

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,11 +1,14 @@
 type User = {
-    _id: number;
+    // _id: number;
     firstName: string;
     lastName: string;
     email: string;
     password: string;
 }
 
-type initialStateType = {
+type InitialStateType = {
     users: User[];
+    user: User | undefined;
+    postUsers: (register:User) =>  void;
+    getUsers: () => void;
 }
